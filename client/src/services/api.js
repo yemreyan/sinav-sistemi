96053009956faab0c9d808a6fa4079a245aff3b2
Yunus Emre Yalciner <emre.yalciner@192.168.1.9>
2026-02-28 22:39:14 +0300

    Update API URL for Render deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'https://sinav-backend.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
